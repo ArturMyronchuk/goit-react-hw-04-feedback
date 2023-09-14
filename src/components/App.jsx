b3b8b576942758417e285dafab03c1c181f0394e
Artur Myronchuk <122921226+ArturMyronchuk@users.noreply.github.com>
2023-09-14 23:52:04 +0300

    fixxx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import FeedbackWidget from './Feedback/feedback';
 import css from './Feedback/fedback.module.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className={css.container}>
-        <FeedbackWidget />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className={css.container}>
+      <FeedbackWidget />
+    </div>
+  );
+};
 
 export default App;
